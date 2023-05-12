6ec6d885118f5ed3257b4430d9a27bf0999311a6
Sandy Maguire <sandy@sandymaguire.me>
2023-05-12 11:47:39 -0700

    bump agda

--- src/Blagda/Agda.hs
+++ src/Blagda/Agda.hs
@@ -9,12 +9,13 @@ module Blagda.Agda where
 import           Agda.Interaction.FindFile (SourceFile(..))
 import           Agda.Interaction.Imports
 import           Agda.Interaction.Options
-import           Agda.Syntax.Abstract
+import           Agda.Syntax.Abstract hiding (Type)
 import           Agda.Syntax.Abstract.Views
 import           Agda.Syntax.Common
-import qualified Agda.Syntax.Concrete as Con
 import           Agda.Syntax.Info
-import           Agda.Syntax.Internal (Type, Dom, domName)
+import           Agda.Syntax.Internal (Type, domName)
+import qualified Agda.Syntax.Internal as I
+import qualified Agda.Syntax.Internal.Generic as I
 import           Agda.Syntax.Position
 import           Agda.Syntax.Scope.Base
 import           Agda.Syntax.Translation.AbstractToConcrete (abstractToConcrete_)
@@ -26,7 +27,7 @@ import           Agda.Utils.Pretty
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Data.Foldable
-import           Data.Generics
+import           Data.Functor.Identity
 import           Data.List
 import           Data.List.NonEmpty (NonEmpty((:|)))
 import           Data.Map.Lazy (Map)
@@ -70,16 +71,12 @@ setupTCM = do
   setCommandLineOptions' absp defaultOptions{optLocalInterfaces = True}
   pure (filePath absp)
 
-killDomainNames :: Type -> Type
-killDomainNames = everywhere (mkT unDomName) where
-  unDomName :: Dom Type -> Dom Type
-  unDomName m = m{ domName = Nothing }
-
-killQual :: Con.Expr -> Con.Expr
-killQual = everywhere (mkT unQual) where
-  unQual :: Con.QName -> Con.QName
-  unQual (Con.Qual _ x) = unQual x
-  unQual x = x
+prettifyTerm :: Type -> Type
+prettifyTerm = runIdentity . I.traverseTermM unDomName where
+  unDomName :: I.Term -> Identity I.Term
+  unDomName (I.Pi d x) = pure $ I.Pi d{domName = Nothing} x
+  unDomName (I.Def q x) = pure $ I.Def q{qnameModule = MName []} x
+  unDomName x = pure x
 
 tcAndLoadPublicNames :: FilePath -> String -> TCMT IO (Map Text Text)
 tcAndLoadPublicNames path basepn = do
@@ -96,12 +93,12 @@ tcAndLoadPublicNames path basepn = do
     case t of
       Left _ -> pure Nothing
       Right d -> do
-        expr <- reify . killDomainNames $ defType d
-        t <- fmap (render . pretty . killQual) .
+        expr <- reify . prettifyTerm $ defType d
+        t <- fmap (render . pretty) .
           abstractToConcrete_ . removeImpls $ expr
 
         case rangeFile (nameBindingSite (qnameName name)) of
-          S.Just (filePath -> f)
+          S.Just (filePath . rangeFilePath -> f)
             | ("Agda/Builtin" `isInfixOf` f) || ("Agda/Primitive" `isInfixOf` f) ->
               pure $ do
                 fp <- fakePath name
